a74dfa778bb67957d26a7e63291a260b31d5a1c2
rioobod <akanbilola@gmail.com>
2022-03-27 17:23:41 -0500

    updated schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2),
+  department_id INTEGER,
+  CONSTRAINT dept_id FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT role__id FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT manager__id FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
