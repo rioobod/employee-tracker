d03cf4411e963d5b6852bb2b1d7ab2949c8c1e9e
rioobod <akanbilola@gmail.com>
2022-04-02 19:38:14 -0500

    modified connectionjs, deleted inputcheck file and updated serverjs

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,13 @@
 const mysql = require('mysql2');
-
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: '',
-      password: '',
-      database: 'employee_tracker'
-      // user: process.env.DB_USER,
-      // password: process.env.DB_PW,
-      // database: process.env.DB_NAME
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME
     },
     console.log('Connected to the Employee Tracker database.')
   );
